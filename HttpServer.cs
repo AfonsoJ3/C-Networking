1d8655c7452d1f1a818b2c9f7934de78f74c5144
AfonsoJ3 <afonsoj3@wit.edu>
2023-04-26 11:07:39 -0400

    new file was create

---
+++ HttpServer.cs
@@ -0,0 +1,12 @@
+/*Is project teach you how to create a database. It is based on a tutorial found on the internet 
+at https://github.com/codecrafters-io/build-your-own-x*/
+// Filename:  Downloader.cs        
+// Author:    Benjamin N. Summerton <define-private-public>        
+// License:   Unlicense (http://unlicense.org/)
+// Student: Jorge Afonso
+
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
